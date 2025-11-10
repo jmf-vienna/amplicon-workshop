6e99e98435bef90ea1b116df27d19dd9c7bf09d6
Bela <75233+sci@users.noreply.github.com>
2025-11-10 11:05:51 +0100

    R packages install script

---
+++ prepare/install.R
@@ -0,0 +1,42 @@
+#!/usr/bin/env Rscript
+
+if (!require("pak", quietly = TRUE)) {
+  install.packages("pak")
+}
+
+pak::pkg_install(
+  c(
+    "tidyverse",
+    "patchwork",
+    "styler",
+    "lintr",
+    "usethis",
+    "seqinr",
+    "vegan",
+    "ggrepel",
+    "ggbeeswarm",
+    "svglite",
+    "rstatix",
+    "ggpubr",
+    # Bioconductor:
+    "BiocVersion",
+    "BiocManager",
+    "dada2",
+    "phyloseq",
+    "ALDEx2",
+    "DESeq2",
+    "decontam",
+    "mia",
+    "miaViz",
+    # GitHub:
+    "david-barnett/microViz",
+    "mikemc/speedyseq",
+    "MadsAlbertsen/ampvis2@*release",
+    "jmf-vienna/jmf-r@*release"
+  ),
+  upgrade = TRUE
+)
+
+if (packageVersion("BiocVersion") < "3.22") {
+  BiocManager::install(version = "3.22")
+}
